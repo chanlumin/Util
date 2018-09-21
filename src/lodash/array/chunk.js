1bc0084eaed12f00cbd1822fe2a69834a2ba7542
unknown <chenlumin@360buyad.local>
2018-09-21 10:09:43 +0800

    调整chunk循环的下标和初始化的参数的位置

--- src/lodash/array/chunk.js
+++ src/lodash/array/chunk.js
@@ -1,20 +1,20 @@
 import {slice} from  '../base/index'
 
 const chunk = (arr, chunkSize) => {
+  let length = arr == null ? 0 : arr.length
 
   // 1 chunkSize不传默认是1
   chunkSize = chunkSize ? chunkSize : 1 
-  if(!arr || chunkSize < 0) return []
 
-  let length = arr.length || 0 
-  // 3/2 = 1 
+  // 2 chunkSize 等于0的时候也返回空数组 
+  if(!arr.length || chunkSize < 1) return []
+
   const chunkLength = Math.ceil(length / chunkSize) 
 
   let result = new Array(chunkLength)
-  console.log(chunkLength,'xxx')
-  for(let i = 0, j = 0; i < chunkLength; j+=chunkSize,i++) {
-    // 2. slice会自动处理最后一部分的元素  如果i+chunkSize的end 超出范围的话自动取到最后一个元素
-    result[i] = slice(arr, j, j+chunkSize)
+  for(let i = 0,resIndex=0; i < length; i+=chunkSize) {
+    // 2. slice会自动处理最后一部分的元i素  如果i+chunkSize的end 超出范围的话自动取到最后一个元素
+    result[resIndex++] = slice(arr, i, i+chunkSize)
   }
   return result
 }
